933db60e6cf32532ffb6deca6eef741fafe00891
Ken Wu <ken594088@gmail.com>
2019-03-12 11:50:09 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -9,16 +9,16 @@ SET[1]=vimrc
 SET[2]=bashrc
 SET[3]=zshrc
 
-# install repo
+# install
 sudo apt-get install -y vim zsh git
 
 # download &install oh-my-zsh
 git clone ${URL1}
 ./oh-my-zsh/tools/install.sh
-mv ./${theme} ~/.oh-my-zsh/themes
+mv ./${theme} ${HOME}/.oh-my-zsh/themes
 
 # set env
 for cnt in $(seq 1 3)
 do
-	mv ./${SET[${cnt}]} ~/.${SET[${cnt}]}
+	mv ./${SET[${cnt}]} ${HOME}/.${SET[${cnt}]}
 done
