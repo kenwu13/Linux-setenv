6cf517f6fa95814641df047edd1ea6dd48a4fdd8
Ken Wu <ken594088@gmail.com>
2019-03-12 11:42:47 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -2,25 +2,23 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-URL1=https://github.com/kenwu13/Linux.git
-URL2=https://github.com/robbyrussell/oh-my-zsh.git
+URL1=https://github.com/robbyrussell/oh-my-zsh.git
 theme=kenwu.zsh-theme
 
 SET[1]=vimrc
 SET[2]=bashrc
 SET[3]=zshrc
 
+# install repo
 sudo apt-get install -y vim zsh git
+
+# download &install oh-my-zsh
 git clone ${URL1}
-git clone ${URL2}
 ./oh-my-zsh/tools/install.sh
+mv ./${theme} ~/.oh-my-zsh/themes
 
-mv ./Linux/${theme} ~/.oh-my-zsh/themes
-
+# set env
 for cnt in $(seq 1 3)
 do
-	mv ./Linux-setenv/${SET[${cnt}]} ~/.${SET[${cnt}]}
+	mv ./${SET[${cnt}]} ~/.${SET[${cnt}]}
 done
-
-rm -rf ./Linux-setenv
-rm -rf ./oh-my-zsh
